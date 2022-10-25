42adfd7da80b674035af554771787ceeea0287f1
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-10-26 02:12:37 +0600

    added data

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
+app.use(cors());
+
 const port = process.env.PORT || 5000;
 
 
 
 app.get('/', (req, res) => {
-    res.send('eLearning API runnig');
-  });
+  res.send('eLearning API runnig');
+});
 
-  app.listen(port, () => {
-    console.log("eLearning server running on port", port);
-  })
+app.listen(port, () => {
+  console.log("eLearning server running on port", port);
+})
